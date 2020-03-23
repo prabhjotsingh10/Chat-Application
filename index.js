70b2620350f102b206d151e3afa92469d71ca282
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2020-03-23 19:43:58 -0400

    Added styling, javascript and adjustments

---
+++ index.js
@@ -0,0 +1,15 @@
+// const click = () => {
+//     document.getElementById("stay_cntd").innerHTML = `<h3> Start a conversation </h3>
+//     <p> Send Hello to your friend right away!`
+// } 
+
+
+document.querySelector("#details").addEventListener("click", ()=>{
+    document.getElementById("stay_cntd").innerHTML = `<h1 class="strt_cnvo"> Start a Conversation </h1>
+    <p> Send hello to your friend ! </p>
+    <div class="msg_div">
+    <button class="snd_msg"> <i class="far fa-grin"></i> </button>
+    <textarea class="msgbox" placeholder ="Say Hi..."></textarea>
+    <button class="snd_msg"> <i class="fas fa-paper-plane"></i> </button>
+    </div>`
+})
