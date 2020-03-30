e8aba26a23d2192befd046ac33319bb726040ab2
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2020-03-30 15:06:57 -0400

    Added React Layout to HTML and CSS

--- js/index.js
+++ js/index.js
@@ -4,12 +4,139 @@
 // } 
 
 
-document.querySelector("#details").addEventListener("click", ()=>{
-    document.getElementById("stay_cntd").innerHTML = `<h1 class="strt_cnvo"> Start a Conversation </h1>
-    <p> Send hello to your friend Pete! </p>
-    <div class="msg_div">
-    <button class="snd_msg"> <i class="far fa-grin"></i> </button>
-    <textarea class="msgbox" placeholder ="Say Hi..."></textarea>
-    <button class="snd_msg"> <i class="fas fa-paper-plane"></i> </button>
-    </div>`
-})
+// document.querySelector("#details").addEventListener("click", ()=>{
+//   document.getElementById("stay_cntd").innerHTML = `<h1 className="strt_cnvo"> Start a Conversation </h1>
+//   <p> Send hello to your friend Pete! </p>
+//   <div className="msg_div">
+//   <button className="snd_msg"> <i className="far fa-grin"></i> </button>
+//   <textarea className="msgbox" placeholder ="Say Hi..."></textarea>
+//   <button className="snd_msg"> <i className="fas fa-paper-plane"></i> </button>
+//   </div>`
+// })
+const Contact = () => {
+  
+    return (
+      <div className="contacts">
+        <div>
+          <ul>
+            <li className="logo_and_usrnme">
+              <div className="chatmate">
+                <h3 className="logo">ChatMate</h3> <h4 className="username">Prabhjot Singh</h4> <a href="#" className="sgn_out">Sign Out <i className="fas fa-sign-out-alt"></i></a>
+              </div>
+            </li>
+            <li>
+              <div className="user_profile">
+                <h3>Direct Messages <i className="fas fa-sort-down"></i></h3>
+              </div>
+            </li>
+            <li className="dm">
+              <div className="cntc_1">
+                <img src="img/img.jpg" alt="Pete" width="55em" height="55em" className="img1" />
+                <div className="details" id="details">
+                  <h3>Pete</h3>
+                  <p>Heyy</p>
+                </div>
+              </div>
+              <hr />
+            </li>
+            <li className="dm">
+              <div className="cntc_1">
+                <img src="img/Jess.jpg" alt="Pete" width="55em" height="55em" className="img1" />
+  
+                <div className="details" id="details1">
+                  <h3>Jess</h3>
+                  <p className="jess_msg">Movie Night tonight?</p>
+                </div>
+              </div>
+              <hr />
+            </li>
+  
+            <li className="dm">
+              <div className="cntc_1">
+                <img src="img/img.jpg" alt="Pete" width="55em" height="55em" className="img1" />
+                <div className="details" id="details2">
+                  <h3>Kevin</h3>
+                  <p>new message</p>
+                </div>
+              </div>
+              <hr />
+            </li>
+  
+            <li className="dm">
+              <div className="cntc_1">
+                <img src="img/img.jpg" alt="Pete" width="55em" height="55em" className="img1" />
+                <div className="details" id="details3">
+                  <h3>Neil</h3>
+                  <p>new message</p>
+                </div>
+              </div>
+              <hr />
+            </li>
+          </ul>
+  
+          <div className="user_profile" id="grp_cnvo">
+            <h3>Group Conversations <i className="fas fa-sort-down"></i></h3>
+          </div>
+  
+          <ul className="grp_chat">
+            <li className="dm">
+              <div className="cntc_1">
+                <img src="img/img.jpg" alt="Pete" width="55em" height="55em" className="img1" />
+                <div className="details">
+                  <h3>Coders United</h3>
+                  <p>What's Up Everybody? </p>
+                </div>
+              </div>
+              <hr />
+            </li>
+  
+            <li className="dm">
+              <div className="cntc_1">
+                <img src="img/img.jpg" alt="Pete" width="55em" height="55em" className="img1" />
+                <div className="details">
+                  <h3>F.R.I.E.N.D.S</h3>
+                  <p>Wanna catch a movie tonight? </p>
+                </div>
+              </div>
+              <hr />
+            </li>
+  
+            <li className="dm">
+              <div className="cntc_1">
+                <img src="img/img.jpg" alt="Pete" width="55em" height="55em" className="img1" />
+                <div className="details">
+                  <h3>Humber Media Pros</h3>
+                  <p>Our assignment is due fellas! Let's get to it!</p>
+                </div>
+              </div>
+              <hr />
+            </li>
+          </ul>
+  
+        </div>
+      </div>
+    )
+  }
+  
+  const StayConnected = () => {
+    return (
+      <div className="stay_connected" id="stay_cntd">
+        <h1 className="sty_cntd">Stay Connected for more messages</h1>
+        <p>The app connects to your phone to sync messages. To reduce data usage, connect your phone to WiFi.</p>
+        <p>Click on a chat to get started.</p>
+      </div>
+    )
+  }
+  
+  const App = () => {
+  
+    return (
+      <div className="app">
+        <Contact />
+        <StayConnected />
+      </div>
+    )
+  
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
